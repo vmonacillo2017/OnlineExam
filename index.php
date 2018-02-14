96c85c6de50d571ce5b24c335732cd507c638950
vmonacillo2017 <34480196+vmonacillo2017@users.noreply.github.com>
2018-02-14 23:35:45 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -12,15 +12,15 @@
  <script src="js/jquery.js" type="text/javascript"></script>
 
   <script src="js/bootstrap.min.js"  type="text/javascript"></script>
- 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-<?php if(@$_GET['w'])
-{echo'<script>alert("'.@$_GET['w'].'");</script>';}
-?>
-<script>
-function validateForm() {var y = document.forms["form"]["name"].value;	var letters = /^[A-Za-z]+$/;if (y == null || y == "") {alert("Name must be filled out.");return false;}var z =document.forms["form"]["college"].value;if (z == null || z == "") {alert("college must be filled out.");return false;}var x = document.forms["form"]["email"].value;var atpos = x.indexOf("@");
-var dotpos = x.lastIndexOf(".");if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {alert("Not a valid e-mail address.");return false;}var a = document.forms["form"]["password"].value;if(a == null || a == ""){alert("Password must be filled out");return false;}if(a.length<5 || a.length>25){alert("Passwords must be 5 to 25 characters long.");return false;}
-var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords must match.");return false;}}
-</script>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+  <?php if(@$_GET['w'])
+  {echo'<script>alert("'.@$_GET['w'].'");</script>';}
+  ?>
+  <script>
+  function validateForm() {var y = document.forms["form"]["name"].value;  var letters = /^[A-Za-z]+$/;if (y == null || y == "") {alert("Name must be filled out.");return false;}var z =document.forms["form"]["college"].value;if (z == null || z == "") {alert("college must be filled out.");return false;}var x = document.forms["form"]["email"].value;var atpos = x.indexOf("@");
+  var dotpos = x.lastIndexOf(".");if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {alert("Not a valid e-mail address.");return false;}var a = document.forms["form"]["password"].value;if(a == null || a == ""){alert("Password must be filled out");return false;}if(a.length<5 || a.length>25){alert("Passwords must be 5 to 25 characters long.");return false;}
+  var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords must match.");return false;}}
+  </script>
 
 
 </head>
@@ -68,7 +68,7 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
       <div class="modal-footer">
         <button type="button" class="btn btn-info" data-dismiss="modal">Cancel</button>
         <button type="submit" class="btn btn-primary">Log in</button>
-		</fieldset>
+    </fieldset>
 </form>
       </div>
     </div><!-- /.modal-content -->
@@ -80,12 +80,106 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
 </div>
 
 <div class="bg1">
-  <div class="row">
+  <div class="container">
 
-    <div class="col-md-3"></div>
-      <div class="col-md-4 panel">
-  <!-- registration form begins -->  
-        <form class="form-horizontal" name="form" action="sign.php?q=account.php" onSubmit="return validateForm()" method="POST">
+    <div style="background-color: rgba(255,255,255,0.8); text-align: center;"  class="col-2 text-truncate">
+      
+      <h1>Welcome</h1>
+      <h2>CNU Online Entrance Examination Portal</h2>
+      <p class="text-justify">  This portal is created in order to serve the pulic for easy access on the entrance examination of Cebu Normal University. 
+      Please ensure that all your requirements are submitted to the "Testing Center". This exam will test your IQ to qualify for enrolment of the course you applied for.
+      Be sure to provide correct information in the registration form because this will be the basis for enrolment if you qualify. This examination requires you to finish within thirty (30) minutes. Good Luck!   </p>
+    </div>
+      
+    </div>
+  </div>
+</div><!--container end-->
+
+<!--Footer start-->
+
+<div class="row footer">
+<div class="col-md-3 box">
+<a href="http://cnu.edu.ph" target="_blank">About Us</a>
+</div>
+<div class="col-md-3 box">
+<a href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
+<div class="col-md-3 box">
+<a href="#" data-toggle="modal" data-target="#developers">Developers</a>
+</div>
+<div class="col-md-3 box">
+<a href="#" data-toggle="modal" data-target="#regs">Register</a>
+</div>
+
+<!-- Modal For Developers-->
+<div class="modal fade title1" id="developers">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" style="font-family:'typo' "><span style="color:orange">Developers</span></h4>
+      </div>
+    
+      <div class="modal-body">
+        <p>
+    <div class="row">
+    <div class="col-md-4">
+     <img src="image/Vinz01.jpg" width=100 height=100 alt="Vincent Monacillo" class="img-rounded">
+     </div>
+     <div class="col-md-5">
+    <a href="https://www.facebook.com/vinz.monacillo" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">Vincent F. Monacillo</a>
+    <h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+63 9778898494</h4>
+    <h4 stylDeveloperse="font-family:'typo' ">vmonacillo@gmail.com</h4>
+    <h4 style="font-family:'typo' ">Cebu Normal University</h4></div></div>
+    </p>
+      </div>
+    
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!--Modal for admin login-->
+   <div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">Log in</span></h4>
+      </div>
+      <div class="modal-body title1">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<form role="form" method="post" action="admin.php?q=index.php">
+<div class="form-group">
+<input type="text" name="uname" maxlength="20"  placeholder="Admin user id" class="form-control"/> 
+</div>
+<div class="form-group">
+<input type="password" name="password" maxlength="15" placeholder="Password" class="form-control"/>
+</div>
+<div class="form-group" align="center">
+<input type="submit" name="login" value="Login" class="btn btn-primary" />
+</div>
+</form>
+</div><div class="col-md-3"></div></div>
+      </div>
+      <!--<div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>-->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!-- Modal For Developers-->
+<div class="modal fade title1" id="regs">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" style="font-family:'typo' "><span style="color:orange">Register Here!</span></h4>
+      </div>
+    
+      <div class="modal-body">
+        <form class="form-horizontal" name="form" action="sign.php?q=myaccount.php" onSubmit="return validateForm()" method="POST">
         <fieldset>
 
           <!-- Text input-->
@@ -165,80 +259,8 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
 
         </fieldset>
         </form>
-      </div><!--col-md-6 end-->
-    </div>
-  </div>
-</div><!--container end-->
-
-<!--Footer start-->
-<div class="container">
-<div class="row footer">
-<div class="col-md-3 box">
-<a href="#" target="_blank">About us</a>
-</div>
-<div class="col-md-3 box">
-<a href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
-<div class="col-md-3 box">
-<a href="#" data-toggle="modal" data-target="#developers">Developers</a>
-</div>
-<div class="col-md-3 box">
-<a href="feedback.php" target="_blank">Feedback</a></div></div>
-</div>
-<!-- Modal For Developers-->
-<div class="modal fade title1" id="developers">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" style="font-family:'typo' "><span style="color:orange">Developers</span></h4>
       </div>
     
-      <div class="modal-body">
-        <p>
-    <div class="row">
-    <div class="col-md-4">
-     <img src="image/Vinz01.jpg" width=100 height=100 alt="Vincent Monacillo" class="img-rounded">
-     </div>
-     <div class="col-md-5">
-    <a href="https://www.facebook.com/vinz.monacillo" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">Vincent F. Monacillo</a>
-    <h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+63 9778898494</h4>
-    <h4 stylDeveloperse="font-family:'typo' ">vmonacillo@gmail.com</h4>
-    <h4 style="font-family:'typo' ">Cebu Normal University</h4></div></div>
-    </p>
-      </div>
-    
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<!--Modal for admin login-->
-	 <div class="modal fade" id="login">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">LOGIN</span></h4>
-      </div>
-      <div class="modal-body title1">
-<div class="row">
-<div class="col-md-3"></div>
-<div class="col-md-6">
-<form role="form" method="post" action="admin.php?q=index.php">
-<div class="form-group">
-<input type="text" name="uname" maxlength="20"  placeholder="Admin user id" class="form-control"/> 
-</div>
-<div class="form-group">
-<input type="password" name="password" maxlength="15" placeholder="Password" class="form-control"/>
-</div>
-<div class="form-group" align="center">
-<input type="submit" name="login" value="Login" class="btn btn-primary" />
-</div>
-</form>
-</div><div class="col-md-3"></div></div>
-      </div>
-      <!--<div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>-->
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
