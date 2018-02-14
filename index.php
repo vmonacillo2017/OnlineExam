28cb08d9673eb5727fd0c0f8130a12a59405fd9e
vmonacillo2017 <34480196+vmonacillo2017@users.noreply.github.com>
2018-02-14 23:34:28 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -12,7 +12,16 @@
  <script src="js/jquery.js" type="text/javascript"></script>
 
   <script src="js/bootstrap.min.js"  type="text/javascript"></script>
-  <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+<?php if(@$_GET['w'])
+{echo'<script>alert("'.@$_GET['w'].'");</script>';}
+?>
+<script>
+function validateForm() {var y = document.forms["form"]["name"].value;	var letters = /^[A-Za-z]+$/;if (y == null || y == "") {alert("Name must be filled out.");return false;}var z =document.forms["form"]["college"].value;if (z == null || z == "") {alert("college must be filled out.");return false;}var x = document.forms["form"]["email"].value;var atpos = x.indexOf("@");
+var dotpos = x.lastIndexOf(".");if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {alert("Not a valid e-mail address.");return false;}var a = document.forms["form"]["password"].value;if(a == null || a == ""){alert("Password must be filled out");return false;}if(a.length<5 || a.length>25){alert("Passwords must be 5 to 25 characters long.");return false;}
+var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords must match.");return false;}}
+</script>
+
 
 </head>
 
@@ -59,7 +68,7 @@
       <div class="modal-footer">
         <button type="button" class="btn btn-info" data-dismiss="modal">Cancel</button>
         <button type="submit" class="btn btn-primary">Log in</button>
-    </fieldset>
+		</fieldset>
 </form>
       </div>
     </div><!-- /.modal-content -->
@@ -71,26 +80,101 @@
 </div>
 
 <div class="bg1">
-  <div class="container">
+  <div class="row">
 
-    <div style="background-color: rgba(255,255,255,0.8); text-align: center;"  class="col-2 text-truncate">
-      
-      <h1>Welcome</h1>
-      <h2>CNU Online Entrance Examination Portal</h2>
-      <p class="text-justify">  This portal is created in order to serve the pulic for easy access on the entrance examination of Cebu Normal University. 
-      Please ensure that all your requirements are submitted to the "Testing Center". This exam will test your IQ to qualify for enrolment of the course you applied for.
-      Be sure to provide correct information in the registration form because this will be the basis for enrolment if you qualify. This examination requires you to finish within thirty (30) minutes. Good Luck!   </p>
-    </div>
-      
+    <div class="col-md-3"></div>
+      <div class="col-md-4 panel">
+  <!-- registration form begins -->  
+        <form class="form-horizontal" name="form" action="sign.php?q=account.php" onSubmit="return validateForm()" method="POST">
+        <fieldset>
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-md-12 control-label" for="name"></label>  
+            <div class="col-md-12">
+            <input id="name" name="name" placeholder="Enter your name" class="form-control input-md" type="text">
+              
+            </div>
+          </div>
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-md-12 control-label" for="gender"></label>
+            <div class="col-md-12">
+              <select id="gender" name="gender" placeholder="Enter your gender" class="form-control input-md" >
+             <option value="Male">Select Gender</option>
+            <option value="M">Male</option>
+            <option value="F">Female</option> </select>
+            </div>
+          </div>
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-md-12 control-label" for="name"></label>  
+            <div class="col-md-12">
+            <input id="college" name="college" placeholder="Enter your previous school attended" class="form-control input-md" type="text">
+              
+            </div>
+          </div>
+
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-md-12 control-label title1" for="email"></label>
+            <div class="col-md-12">
+              <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
+              
+            </div>
+          </div>
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-md-12 control-label" for="mob"></label>  
+            <div class="col-md-12">
+            <input id="mob" name="mob" placeholder="Enter your mobile number" class="form-control input-md" type="number">
+              
+            </div>
+          </div>
+
+
+          <!-- Text input-->
+          <div class="form-group">
+            <label class="col-md-12 control-label" for="password"></label>
+            <div class="col-md-12">
+              <input id="password" name="password" placeholder="Enter your password" class="form-control input-md" type="password">
+              
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="col-md-12control-label" for="cpassword"></label>
+            <div class="col-md-12">
+              <input id="cpassword" name="cpassword" placeholder="Confirm Password" class="form-control input-md" type="password">
+              
+            </div>
+          </div>
+          <?php if(@$_GET['q7'])
+          { echo'<p style="color:red;font-size:15px;">'.@$_GET['q7'];}?>
+          <!-- Button -->
+          <div class="form-group">
+            <label class="col-md-12 control-label" for=""></label>
+            <div class="col-md-12"> 
+              <input  type="submit" class="sub" value="Register" class="btn btn-primary"/>
+            </div>
+          </div>
+
+        </fieldset>
+        </form>
+      </div><!--col-md-6 end-->
     </div>
   </div>
 </div><!--container end-->
 
 <!--Footer start-->
-
+<div class="container">
 <div class="row footer">
 <div class="col-md-3 box">
-<a href="http://cnu.edu.ph" target="_blank">About Us</a>
+<a href="#" target="_blank">About us</a>
 </div>
 <div class="col-md-3 box">
 <a href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
@@ -98,9 +182,8 @@
 <a href="#" data-toggle="modal" data-target="#developers">Developers</a>
 </div>
 <div class="col-md-3 box">
-<a href="./register.php" data-toggle="modal" target="_blank">Register</a>
+<a href="feedback.php" target="_blank">Feedback</a></div></div>
 </div>
-
 <!-- Modal For Developers-->
 <div class="modal fade title1" id="developers">
   <div class="modal-dialog">
@@ -129,12 +212,12 @@
 </div><!-- /.modal -->
 
 <!--Modal for admin login-->
-   <div class="modal fade" id="login">
+	 <div class="modal fade" id="login">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">Log in</span></h4>
+        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">LOGIN</span></h4>
       </div>
       <div class="modal-body title1">
 <div class="row">
