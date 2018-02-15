6f26d66a128afaa8f072791359e4a2f899130c32
vmonacillo2017 <34480196+vmonacillo2017@users.noreply.github.com>
2018-02-16 00:22:17 +0800

    Add files via upload

--- dash.php
+++ dash.php
@@ -413,6 +413,85 @@ echo '</table></div></div>';
 }
 ?>
 
+<!-- upload quiz -->
+<?php if(@$_GET['q']==6) {
+echo '<div id="wrap">
+        <div class="container">
+            <div class="row">
+ 
+                <form class="form-horizontal" action="functions.php" method="post" name="upload_excel" enctype="multipart/form-data">
+                    <fieldset>
+ 
+                        <!-- Form Name -->
+                        <legend>Upload Exam</legend>
+ 
+                        <!-- File Button -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="filebutton">Select File</label>
+                            <div class="col-md-4">
+                                <input type="file" name="file" id="file" class="input-large">
+                            </div>
+                        </div>
+ 
+                        <!-- Button -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="singlebutton">Upload Employee Info</label>
+                            <div class="col-md-4">
+                                <button type="submit" id="submit" name="Upload" class="btn btn-primary button-loading" data-loading-text="Loading...">Upload</button>
+                            </div>
+                        </div>
+ 
+                    </fieldset>
+                </form>
+ 
+            </div>
+            <?php
+               get_all_records();
+            ?>
+        </div>';
+        $Sql = "SELECT * FROM employeeinfo";
+    $result = mysqli_query($con, $Sql);  
+
+
+    if (mysqli_num_rows($result) > 0) {
+     echo "<div class='table-responsive'><table id='myTable' class='table table-striped table-bordered'>
+             <thead><tr><th>EMP ID</th>
+                          <th>First Name</th>
+                          <th>Last Name</th>
+                          <th>Email</th>
+                          <th>Registration Date</th>
+                        </tr></thead><tbody>";
+
+
+     while($row = mysqli_fetch_assoc($result)) {
+
+         echo "<tr><td>" . $row['emp_id']."</td>
+                   <td>" . $row['firstname']."</td>
+                   <td>" . $row['lastname']."</td>
+                   <td>" . $row['email']."</td>
+                   <td>" . $row['reg_date']."</td></tr>";        
+     }
+    
+     echo "</tbody></table></div>";
+     
+} else {
+     echo "you have no records";
+}
+  /*echo '<div>
+            <form class="form-horizontal" action="functions.php" method="post" name="upload_excel"   
+                      enctype="multipart/form-data">
+                  <div class="form-group">
+                            <div class="col-md-4 col-md-offset-4">
+                                <input type="submit" name="Export" class="btn btn-success" value="Export to Excel"/>
+                            </div>
+                   </div>                    
+            </form>           
+        </div>';*/
+
+
+}
+?>
+
 
 
 
